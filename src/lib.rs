5f1cab2733788245ca185c89d4766f29ada7613c
Denis Gorbachev <829578+DenisGorbachev@users.noreply.github.com>
2025-02-12 16:57:34 +0700

    feat: add docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,24 @@
+//! # Task Management System
+//!
+//! ## Basics
+//!
+//! A task is defined as a function that returns a [`Result`].
+//!
+//! In some cases, it may be convenient to return the [`Result`] from the `Builder` of a struct (represent the task with a struct).
+//!
+//! ## Tasks-as-types
+//!
+//! Rust has strong static types. These types can be seen as "tasks" that need to be completed. The tasks can be "completed" by returning a value of a specific type. If a type represents a task, then a value represents a result of the completion of that task.
+//!
+//! In other words:
+//!
+//! * A task is defined as a Rust type.
+//! * A task with multiple dependencies is defined as `struct` (each dependency becomes a struct field).
+//! * A task with multiple ways to complete is defined as `enum` (each way to complete becomes an enum variant).
+//! * A task result is defined as a value of a Rust type.
+//! * A task result is produced by a [Builder](https://crates.io/crates/derive_builder).
+//! * If the builder returns an error, this error can be displayed to a human that should complete the task.
+
 pub fn add(left: u64, right: u64) -> u64 {
     left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
 }
